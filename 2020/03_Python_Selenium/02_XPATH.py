c369c2d74708b728571cb4d0ef71cc6c24c222a2
miguel <miguel.junior.mat@hotmail.com>
2020-02-12 17:06:19 -0300

    Selenium finalizando By selectors

---
+++ 2020/03_Python_Selenium/02_XPATH.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+#Fonte Youtube:https://www.youtube.com/watch?v=PqSXxrVNpLA&list=PLK7KNOA7vbPOK1Ih3SXyaM1VnWut5Tn17&index=4
+
+
+
+
+
+
+
