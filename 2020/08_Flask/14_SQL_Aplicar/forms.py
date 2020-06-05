da3c73db9e35ec766830d51f8f9ec6eff8088c3b
miguel <miguel.junior.mat@hotmail.com>
2020-06-04 22:59:48 -0300

    database aplication sql flask

---
+++ 2020/08_Flask/14_SQL_Aplicar/forms.py
@@ -0,0 +1,18 @@
+# Forms.py
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    IntegerField,
+    SubmitField
+)
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+
+class DelForm(FlaskForm):
+
+    id = IntegerField('Id number of Puppy to Remove: ')
+    submit = SubmitField('Remove Puppy')
