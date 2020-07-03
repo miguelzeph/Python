0f8882d5dd65ef40d7f652731ed3c04e6c8d4561
miguel <miguel.junior.mat@hotmail.com>
2020-07-03 15:34:01 -0300

    django criando super user

--- 2020/11_Django/blog/blog/settings.py
+++ 2020/11_Django/blog/blog/settings.py
@@ -103,12 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
+# --- Moedas, multiplos idiomas, formato de dados ponto ou virgula etc ...
 USE_I18N = True
-
 USE_L10N = True
 
 USE_TZ = True
