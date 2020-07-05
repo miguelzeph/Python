f2b29481b4c9cb6da87dcd24485d69edcddcc5a8
miguel <miguel.junior.mat@hotmail.com>
2020-07-04 22:26:26 -0300

    Sistema de Templates Django

--- 2020/11_Django/blog/website/views.py
+++ 2020/11_Django/blog/website/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_blog(request):
-    return render(request, 'index.html')
+
+    lista = [
+        'Python',
+        'Git',
+        'Django'
+    ]
+
+    data = {'name':'Curso de Django 3', 'lista_tecnologia':lista}
+
+    return render(request, 'index.html',data)
