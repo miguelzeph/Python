64ea87def63e86443582a6f6427c3096374c8639
miguel <miguel.junior.mat@hotmail.com>
2020-07-12 19:13:22 -0300

    finalizado Login

--- 2020/08_Flask/18_login_register/run.py
+++ 2020/08_Flask/18_login_register/run.py
@@ -9,9 +9,16 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     log = False
-    if 'log' in session:
+    nome = '...'
+    if 'log' in session and 'nome' in session:
         log = True
-    return render_template('index.html', log = log)
+        nome = session['nome']
+        
+    return render_template(
+         'index.html',
+         log = log,
+         nome = nome,
+         )
 
 @app.route('/registrar')
 def registrar():
@@ -23,47 +30,39 @@ def criar():
         usuario = mongo.db.usuario # Cria uma Tabela "usuario" dentro do #TESTE_LOGIN
         add = {}
         for parametro in request.form:
-            add[str(parametro)] = request.form[str(parametro)]
-        
-        
+            add[parametro] = request.form[parametro]
         if usuario.find_one({'nome':add['nome']}) is None:
             if add['senha'] == add['senha_confirmar']:
-                
                 imagem = request.files['imagem']
                 mongo.save_file(imagem.filename,imagem)
                 add['imagem'] = imagem.filename
-
                 # Salvar informações
-                usuario.insert(add)
-                
+                usuario.insert(add)    
             else:
                 flash('Confirmação de senha inválida','danger')
-                return redirect(url_for('registrar'))   
-            
+                return redirect(url_for('registrar'))    
         else:
             flash('Já existe este usuário','danger')
             return redirect(url_for('registrar'))
-
     return redirect(url_for('login'))
 
 @app.route('/login', methods = ['POST','GET'])
 def login():
     if request.method == 'POST':
-        
         usuario = mongo.db.usuario
         if usuario.find_one({'nome':request.form['nome']}) is None:
             flash('Usuário não cadastrado','danger')
         else:
+
             usuario_nome = usuario.find_one({'nome':request.form['nome']})
-            
             if usuario_nome['senha'] == request.form['senha']:
                 session['log'] = True
-                flash('LOGADO','danger')
+                session['nome'] = usuario_nome['nome']
+                session['imagem_perfil'] = usuario_nome['imagem']
+                flash('LOGADO','success')
                 return redirect(url_for('index'))
             else:
-                
                 flash('Senha Incorreta','danger') 
-    
     return render_template('login.html')
 
 @app.route('/logout')
@@ -72,7 +71,9 @@ def logout():
     session.pop('log') # Remove um
     return render_template('index.html')
 
-
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
